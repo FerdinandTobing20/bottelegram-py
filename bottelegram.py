f46d83c3b6057e3ea87c8cacedb870e5bef1a206
FerdinandTobing20 <jinruncaandell@gmail.com>
2025-06-17 18:10:23 +0700

    Add bot with polling

--- bottelegram.py
+++ bottelegram.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder
+import os
 
-TOKEN = "8171717792:AAFX9308xlzCN0nquPZrH9rHmaWvHFdeQj8"
+# Mengambil API Token dari Environment Variable
+TOKEN = os.getenv("8171717792:AAFX9308xlzCN0nquPZrH9rHmaWvHFdeQj8")
 
 async def start(update: Update, context):
     await update.message.reply_text("Hallo! Bot aktif dan siap melayani 😊")
@@ -10,11 +12,14 @@ async def echo(update: Update, context):
     await update.message.reply_text(f"Anda mengatakan: {update.message.text}")
 
 def main():
+    # Membangun application
     app = ApplicationBuilder().token(TOKEN).build()
 
+    # Menambah handlers
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
+    # Menjalankan bot secara polling
     app.run_polling()
 
 
